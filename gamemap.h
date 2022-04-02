9237853e95bfa48cd02d7b0b24078038f802c91a
Jiang Zhejun <jasonjiang03@outlook.com>
2022-04-02 17:50:18 +0800

    init commit for gamemap

---
+++ gamemap.h
@@ -0,0 +1,22 @@
+// gamemap.h
+#include <string>
+#include <vector>
+
+class gamemap {
+public:
+    void init_map();
+    void output_map();
+    void element_gen();
+    std::vector<std::vector<std::string>> get_map_content(std::vector<std::vector<std::string>>);
+
+private:
+    // Every game character occupies 2 places: 
+    // ꆜ
+    // ██
+    // tt
+    // Therefore, each block in the game is designed to occupy two places. 
+    // The data type of vector<vector<string>> is used to store the contents of the map
+    // The map size will be a 40*20 2D array of string
+    std::vector<std::vector<std::string>> map_content;
+
+};
